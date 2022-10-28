d07c4cc65a34cec4f9633cbfb5f327625ca0ca25
Thongzxje <thonghoangdanh@gmail.com>
2022-10-28 23:18:16 +0300

    Implemented Token

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -23,6 +24,7 @@ app.use(express.json());
 
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/notes', notesRouter);
 
--- api/controllers/notes.js
+++ api/controllers/notes.js
@@ -2,10 +2,18 @@ const notesRouter = require('express').Router();
 const { response } = require('../app');
 const Note = require('../models/note')
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 notesRouter.get('/', async (req, res) => {
-    const notes = await Note.find({});
-    console.log(notes);
+    const notes = await Note.find({}).populate('user', { username: 1, name: 1});
     res.json(notes);
 })
 
@@ -20,22 +28,26 @@ notesRouter.get('/:id', (req, res, next) => {
         }).catch(err => next(err))
 })
 
-notesRouter.post('/', async (req, res, next) => {
-    const body = req.body;
-
-    const user = await User.findById(body.userId);
-
+notesRouter.post('/', async (request, response) => {
+    const body = request.body;
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
+    const user = await User.findById(decodedToken.id);
     const note = new Note({
         text: body.text,
+        important: body.important === undefined ? false : body.important,
         date: new Date(),
         user: user._id
     })
 
-    const savedNote = await note.save();
-    user.notes = user.notes.concat(savedNote._id);
-    await user.save();
+    const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
 
-    res.json(savedNote);
+    response.json(savedNote)
 })
 
 notesRouter.delete('/:id', (req, res, next) => {
